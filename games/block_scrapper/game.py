4f36251a18ef2e3a0f63217fb0e75e009438e641
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-03-05 11:24:36 +0900

    add block class and modified name to game.py

--- games/block_scrapper/game.py
+++ games/block_scrapper/game.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import pygame
 from pygame.locals import *
 import math
@@ -19,13 +20,35 @@ class Paddle(pygame.sprite.Sprite):
         self.rect.clamp_ip(SCREEN)
 
 
+# ブロックのクラス
+class Block(pygame.sprite.Sprite):
+    def __init__(self, filename, x, y):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.image = pygame.image.load(filename).convert()
+        self.rect = self.image.get_rect()
+        # ブロックの左上座標
+        self.rect.left = SCREEN.left + x * self.rect.width
+        self.rect.top = SCREEN.top + y * self.rect.height
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode(SCREEN.size)
+    # 描画用のスプライトグループ
     group = pygame.sprite.RenderUpdates()
+
+    # 衝突判定用のスプライトグループ
+    blocks = pygame.sprite.Group()
+
+    # add to sprite group
     Paddle.containers = group
-    print('a')
+    Block.containers = group, blocks
+
+    # create paddle and blocks
     paddle = Paddle("paddle.png")
+    for x in range(1, 15):
+        for y in range(1, 11):
+            Block("block.png", x, y)
     clock = pygame.time.Clock()
 
     while (1):
