670fdda02b4a0d88c27698defc7493cf6cff6d23
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-03-05 12:34:30 +0900

    modified start to True if pad is clicked

--- games/block_scrapper/game.py
+++ games/block_scrapper/game.py
@@ -4,6 +4,7 @@ from pygame.locals import *
 import math
 import sys
 import pygame.mixer
+
 SCREEN = Rect(0, 0, 400, 400)
 
 
@@ -14,8 +15,9 @@ class Paddle(pygame.sprite.Sprite):
         self.image = pygame.image.load(filename).convert()
         self.rect = self.image.get_rect()
         self.rect.bottom = SCREEN.bottom - 20
+        self.update = self.move
 
-    def update(self):
+    def move(self):
         self.rect.centerx = pygame.mouse.get_pos()[0]
         self.rect.clamp_ip(SCREEN)
 
@@ -48,65 +50,66 @@ class Ball(pygame.sprite.Sprite):
         self.hit = 0
 
     def start(self):
-            # ボールの初期位置(パドルの上)
-            self.rect.centerx = self.paddle.rect.centerx
-            self.rect.bottom = self.paddle.rect.top
-            # 左クリックでボール射出
-            if pygame.mouse.get_pressed()[0] == 1:
-                self.dx = 0
-                self.dy = -self.speed
-            self.update = self.mov
+
+        # ボールの初期位置(パドルの上)
+        self.rect.centerx = self.paddle.rect.centerx
+        self.rect.bottom = self.paddle.rect.top
+        # 左クリックでボール射出
+        if pygame.mouse.get_pressed()[0]:
+            self.dx = 0
+            self.dy = -self.speed
+            self.update = self.move
 
     def move(self):
-            self.rect.centerx += self.dx
-            self.rect.centery += self.dy
-            # 壁との反射
-            if self.rect.left < SCREEN.left:  # 左側
-                self.rect.left = SCREEN.left
-                self.dx = -self.dx  # 速度を反転
-            if self.rect.right > SCREEN.right:  # 右側
-                self.rect.right = SCREEN.right
-                self.dx = -self.dx
-            if self.rect.top < SCREEN.top:  # 上側
-                self.rect.top = SCREEN.top
-                self.dy = -self.dy
-            # パドルとの反射(左端:135度方向, 右端:45度方向, それ以外:線形補間)
-            if self.rect.colliderect(self.paddle.rect) and self.dy > 0:
-                self.hit = 0  # 連続ヒットを0に戻す
-                (x1, y1) = (self.paddle.rect.left - self.rect.width, self.angle_left)
-                (x2, y2) = (self.paddle.rect.right, self.angle_right)
-                x = self.rect.left  # ボールが当たった位置
-                y = (float(y2 - y1) / (x2 - x1)) * (x - x1) + y1  # 線形補間
-                angle = math.radians(y)  # 反射角度
-                self.dx = self.speed * math.cos(angle)
-                self.dy = -self.speed * math.sin(angle)
-            # ボールを落とした場合
-            if self.rect.top > SCREEN.bottom:
-                self.update = self.start  # ボールを初期状態に
-                self.hit = 0
+        self.rect.centerx += self.dx
+        self.rect.centery += self.dy
+        # 壁との反射
+        if self.rect.left < SCREEN.left:  # 左側
+            self.rect.left = SCREEN.left
+            self.dx = -self.dx  # 速度を反転
+        if self.rect.right > SCREEN.right:  # 右側
+            self.rect.right = SCREEN.right
+            self.dx = -self.dx
+        if self.rect.top < SCREEN.top:  # 上側
+            self.rect.top = SCREEN.top
+            self.dy = -self.dy
+        # パドルとの反射(左端:135度方向, 右端:45度方向, それ以外:線形補間)
+        if self.rect.colliderect(self.paddle.rect) and self.dy > 0:
+            self.hit = 0  # 連続ヒットを0に戻す
+            (x1, y1) = (self.paddle.rect.left - self.rect.width, self.angle_left)
+            (x2, y2) = (self.paddle.rect.right, self.angle_right)
+            x = self.rect.left  # ボールが当たった位置
+            y = (float(y2 - y1) / (x2 - x1)) * (x - x1) + y1  # 線形補間
+            angle = math.radians(y)  # 反射角度
+            self.dx = self.speed * math.cos(angle)
+            self.dy = -self.speed * math.sin(angle)
+        # ボールを落とした場合
+        if self.rect.top > SCREEN.bottom:
+            self.update = self.start  # ボールを初期状態に
+            self.hit = 0
 
-            # ボールと衝突したブロックリストを取得
-            blocks_collided = pygame.sprite.spritecollide(self, self.blocks, True)
-            if blocks_collided:  # 衝突ブロックがある場合
-                oldrect = self.rect
-                for block in blocks_collided:
-                    # ボールが左から衝突
-                    if oldrect.left < block.rect.left < oldrect.right < block.rect.right:
-                        self.rect.right = block.rect.left
-                        self.dx = -self.dx
-                    # ボールが右から衝突
-                    if block.rect.left < oldrect.left < block.rect.right < oldrect.right:
-                        self.rect.left = block.rect.right
-                        self.dx = -self.dx
-                    # ボールが上から衝突
-                    if oldrect.top < block.rect.top < oldrect.bottom < block.rect.bottom:
-                        self.rect.bottom = block.rect.top
-                        self.dy = -self.dy
-                    # ボールが下から衝突
-                    if block.rect.top < oldrect.top < block.rect.bottom < oldrect.bottom:
-                        self.rect.top = block.rect.bottom
-                        self.dy = -self.dy
-                    self.hit += 1
+        # ボールと衝突したブロックリストを取得
+        blocks_collided = pygame.sprite.spritecollide(self, self.blocks, True)
+        if blocks_collided:  # 衝突ブロックがある場合
+            oldrect = self.rect
+            for block in blocks_collided:
+                # ボールが左から衝突
+                if oldrect.left < block.rect.left < oldrect.right < block.rect.right:
+                    self.rect.right = block.rect.left
+                    self.dx = -self.dx
+                # ボールが右から衝突
+                if block.rect.left < oldrect.left < block.rect.right < oldrect.right:
+                    self.rect.left = block.rect.right
+                    self.dx = -self.dx
+                # ボールが上から衝突
+                if oldrect.top < block.rect.top < oldrect.bottom < block.rect.bottom:
+                    self.rect.bottom = block.rect.top
+                    self.dy = -self.dy
+                # ボールが下から衝突
+                if block.rect.top < oldrect.top < block.rect.bottom < oldrect.bottom:
+                    self.rect.top = block.rect.bottom
+                    self.dy = -self.dy
+                self.hit += 1
 
 
 def main():
@@ -129,12 +132,12 @@ def main():
     for x in range(1, 15):
         for y in range(1, 11):
             Block("image/block.png", x, y)
-    Ball("ball.png", paddle, blocks)
+    Ball("image/ball.png", paddle, blocks)
 
     clock = pygame.time.Clock()
 
     while (1):
-        clock.tick(30)
+        clock.tick(10)
         screen.fill((0, 20, 0))
         group.update()
         group.draw(screen)
@@ -150,4 +153,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
