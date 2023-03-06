3c585b6c8d03f5281bbd2b7c2cbdd7a31623eed2
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-03-06 19:41:21 +0900

    create draw_figure.py

--- games/tutorial/draw_figure.py
+++ games/tutorial/draw_figure.py
@@ -15,7 +15,9 @@ def main():
         screen.fill((0, 0, 0))
 
         pygame.draw.rect(screen, (255, 255, 0), Rect(10, 10, 300, 200))
-        pygame.
+        pygame.draw.circle(screen, (255, 0, 0), (320, 240), 100)
+        pygame.draw.ellipse(screen, (255, 0, 255), (400, 300, 200, 100))
+        pygame.draw.line(screen, (255, 255, 255), (0, 0), (640, 480))
         pygame.display.update()
 
         for event in pygame.event.get():
