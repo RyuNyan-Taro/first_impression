b797770a74d028029484f0897affc042d82ee843
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-03-07 07:50:02 +0900

    create _tools.py and load_image

---
+++ games/tutorial/_tools.py
@@ -0,0 +1,16 @@
+import pygame
+from pygame.locals import *
+
+
+def load_image(filename, colorkey=None):
+    try:
+        image = pygame.image.load(filename)
+    except pygame.error as message:
+        print("Cannot load image:", filename)
+        raise SystemExit(message)
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image, image.get_rect()
