c00091951763f04b45d1988b30d9736d107dab30
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-03-06 19:56:27 +0900

    create draw_image.py

---
+++ games/tutorial/draw_image.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.locals import *
+import sys
+
+SCREEN_SIZE = (640, 480)
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption('draw_image')
+
+    backimg = pygame.image.load('image/moriyama.jpg')
+    pythonimg = pygame.image.load('image/python.png').convert_alpha()
+
+    while True:
+        screen.blit(backimg, (0, 0))
+        screen.blit(pythonimg, (320, 400))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+
+
+if __name__ == '__main__':
+    main()
