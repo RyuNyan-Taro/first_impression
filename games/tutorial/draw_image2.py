f1f41cb25a5f9fe5457d70c579a1668afca48f85
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-03-06 20:16:56 +0900

    create draw_image2.py

---
+++ games/tutorial/draw_image2.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.locals import *
+import sys
+
+SCREEN_SIZE = (640, 480)
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    pygame.display.set_caption('clear_backscreen')
+
+    planeimg = pygame.image.load('plane.png').convert()
+
+    planeimg2 = pygame.image.load('plane.png').convert()
+    colorkey = planeimg2.get_at((0, 0))
+    planeimg2.set_colorkey(colorkey, RLEACCEL)
+
+    while True:
+        screen.fill((0, 0, 0))
+        screen.blit(planeimg, (100, 100))
+        screen.blit(planeimg2, (200, 100))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
