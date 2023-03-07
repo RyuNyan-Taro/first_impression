44f8a7efdeaab69dc36f4250c09268062472abd3
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-03-07 21:08:20 +0900

    add two images

--- games/tutorial/bound_dogs.py
+++ games/tutorial/bound_dogs.py
@@ -14,11 +14,13 @@ def main():
     nagiimg = pygame.image.load('image/nagi.png').convert_alpha()
     dog_images = (sielimg, nagiimg)
     img_rect = sielimg.get_rect()
+    img_rect2 = sielimg.get_rect()
 
-    vx = vy = 2
+    vx = vy = 1
+    vx2 = vy2 = 3
     clock = pygame.time.Clock()
     img_switch = 0
-    
+
     while True:
         clock.tick(60)
 
@@ -29,9 +31,17 @@ def main():
         if img_rect.top < 0 or img_rect.bottom > SCR_HEIGHT:
             vy = -vy
             img_switch = 0
+        img_rect2.move_ip(vx2, vy2)
+        if img_rect2.left < 0 or img_rect2.right > SCR_WIDTH:
+            vx2 = -vx2
+            img2_switch = 1
+        if img_rect2.top < 0 or img_rect2.bottom > SCR_HEIGHT:
+            vy2 = -vy2
+            img2_switch = 0
 
         screen.fill((0, 0, 255))
         screen.blit(dog_images[img_switch], img_rect)
+        screen.blit(dog_images[img_switch], img_rect2)
         pygame.display.update()
 
         for event in pygame.event.get():
