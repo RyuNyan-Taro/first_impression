2274ff3eddb5a3212345d54209c49336c950004a
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2023-03-18 08:16:33 +0900

    create sprite_group2.py

--- games/tutorial/sprite/sprite_group3.py
+++ games/tutorial/sprite/sprite_group3.py
@@ -27,8 +27,35 @@ def main():
     screen = pygame.display.set_mode(SCR_RECT.size)
     pygame.display.set_caption(u'How to use sprite group 3')
 
-    MySprite.containers = pygame.sprite.RenderUpdates()
+    group = pygame.sprite.RenderUpdates()
+    MySprite.containers = group
+
 
     python1 = MySprite('../image/python.png', 0, 0, 2, 2)
     python2 = MySprite('../image/python.png', 10, 10, 5, 5)
     python3 = MySprite('../image/python.png', 0, 0, 5, 5)
+
+    clock = pygame.time.Clock()
+
+    background = pygame.Surface(SCR_RECT.size)
+    background.fill((0, 0, 255))
+    screen.blit(background, (0, 0))
+    pygame.display.update()
+
+    while True:
+        clock.tick(60)
+        group.clear(screen, background)
+        group.update()
+        print(python1.rect)
+
+        dirty_rects = group.draw(screen)
+        print(dirty_rects)
+
+        pygame.display.update(dirty_rects)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+
+
+if __name__ == '__main__':
+    main()
